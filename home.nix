5c614fab1393da67686cf8bdcb73b56de5d74abb
JD95 <jeffreydwyer95@outlook.com>
2025-11-08 15:03:55 -0800

    Splits out glance config to a separate module

--- home.nix
+++ home.nix
@@ -11,6 +11,8 @@ in {
     (import ./home/sops.nix { inherit home-dir; })
     (import ./home/hyprland.nix)
     (import ./home/yazi.nix)
+    (import ./home/editors.nix)
+    (import ./home/glance.nix)
   ];
 
   home.packages = with pkgs; [
@@ -114,39 +116,6 @@ in {
     };
   };
 
-  services.glance = {
-    enable = true;
-    settings = {
-      server = { port = 8081; };
-      pages = [{
-        name = "Home";
-        columns = [{
-          size = "full";
-          widgets = [
-            {
-              type = "rss";
-              title = "News";
-              feeds = [{ url = "https://www.reddit.com/r/news.rss"; }];
-            }
-            {
-              type = "rss";
-              title = "Politics";
-              feeds = [
-                # Adam Mockler
-                { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC8DA4o0SyaGfyVaBLbF5EXg"; }
-                # Hasan
-                { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCtoaZpBnrd0lhycxYJ4MNOQ"; }
-                # Vaush
-                { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UC1E-JS8L0j1Ei70D9VEFrPQ"; }
-                # The Rational National
-                { url = "https://www.youtube.com/feeds/videos.xml?channel_id=UCo9oQdIk1MfcnzypG3UnURA"; }
-              ];
-            }
-          ];
-        }];
-      }];
-    };
-  };
 
   home.stateVersion = "25.05";
 }
