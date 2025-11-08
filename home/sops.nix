55d069e516fed4870a9aaf2c652a6ee2b27ea4c1
JD95 <jeffreydwyer95@outlook.com>
2025-11-08 15:19:30 -0800

    Fixes sops secrets reference

--- home/sops.nix
+++ home/sops.nix
@@ -6,7 +6,7 @@
 
   sops = {
     age.keyFile = "${home-dir}/.config/sops/age/keys.txt";
-    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFile = ../secrets.yaml;
     secrets = {
       "accounts/google/user" = { };
       "accounts/google/pass" = { };
